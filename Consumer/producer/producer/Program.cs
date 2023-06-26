13c63aed56c50abec567405bacd1032c84dd6bb0
Tsepo Mphongoa <tshepomphongoa@gmail.com>
2023-06-26 22:57:15 +0200

    added the project files
    created both producer and consumer to test the service. they are created using  .net console app

---
+++ Consumer/producer/producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // RabbitMQ connection settings
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        // Create a connection to RabbitMQ
+        using (var connection = factory.CreateConnection())
+        {
+            // Create a channel
+            using (var channel = connection.CreateModel())
+            {
+                string queueName = "myQueue";
+
+                // Declare the queue
+                channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                // Start the producer
+                Console.WriteLine("Producer Microservice - Enter a message to send (or 'exit' to quit):");
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+
+                    if (input.ToLower() == "exit")
+                        break;
+
+                    var body = Encoding.UTF8.GetBytes(input);
+
+                    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+
+                    Console.WriteLine("Sent message: {0}", input);
+                }
+            }
+        }
+    }
+}
---
+++ Consumers/Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // RabbitMQ connection settings
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        // Create a connection to RabbitMQ
+        using (var connection = factory.CreateConnection())
+        {
+            // Create a channel
+            using (var channel = connection.CreateModel())
+            {
+               string queueName = "myQueue";
+
+             
+
+                // Declare the queue
+                channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                // Start the consumer
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Consumer Microservice - Received message: {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                Console.WriteLine("Consumer Microservice - Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
